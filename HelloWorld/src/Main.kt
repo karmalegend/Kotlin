1957e09d932afd5945698a6cf54f03f79eb02198
Bart Ouwerkerk <bart26nl@gmail.com>
2024-07-07 19:50:28 +0200

    Init

---
+++ HelloWorld/src/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val name = "Kotlin"  // val is readonly var is mutable
+    println("Hello, $name !")
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+
+   exercise1()
+}
+
+fun exercise1(){
+    val name = "Mary"
+    val age = 20
+    
+    println("$name is $age years old")
+}
+
