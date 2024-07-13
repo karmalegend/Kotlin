4902ffa18a8603275be2fe2aa1835d91768fdf55
Bart Ouwerkerk <bart26nl@gmail.com>
2024-07-14 00:56:50 +0200

    hibernate mapping feels very error prone.

--- ToDoApi/src/main/kotlin/com/todo/todoapi/domain/ToDoList.kt
+++ ToDoApi/src/main/kotlin/com/todo/todoapi/domain/ToDoList.kt
@@ -7,6 +7,10 @@ data class ToDoList(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_do_list_user_id")
+    val user: ToDoListUser,
     
     // seems like kotlin doesn't have a way to negate magic strings here?
     @OneToMany(mappedBy = "toDoList", cascade = [CascadeType.ALL], orphanRemoval = true)
---
+++ ToDoApi/src/main/kotlin/com/todo/todoapi/domain/ToDoListUser.kt
@@ -0,0 +1,13 @@
+﻿package com.todo.todoapi.domain
+
+import jakarta.persistence.*
+
+@Entity
+data class ToDoListUser( // hibernate can't handle it if we just call it user which feels odd.
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long = 0,
+    
+    @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var toDoList: MutableList<ToDoList>? = mutableListOf()
+) {
+}
