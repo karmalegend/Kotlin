9f8f8ea07250340cd1bb25cd9f1428b626bdf51a
Bart Ouwerkerk <bart26nl@gmail.com>
2024-07-13 15:28:31 +0200

    Use coroutines for asynchronous data fetching in ToDoListController.
    The ToDoListController has been updated to use Kotlin coroutines for fetching data asynchronously, improving overall performance. Corresponding dependencies have been added in the build.gradle file to support this change.

--- ToDoApi/src/main/kotlin/com/todo/todoapi/controllers/ToDoListController.kt
+++ ToDoApi/src/main/kotlin/com/todo/todoapi/controllers/ToDoListController.kt
@@ -2,15 +2,21 @@
 
 import com.todo.todoapi.dataAccess.ToDoListRepository
 import com.todo.todoapi.domain.ToDoList
-import org.springframework.beans.factory.annotation.Autowired
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/ToDoList")
-class ToDoListController(@Autowired private val repository: ToDoListRepository) {
-    
-    @GetMapping fun getList(): List<ToDoList> = repository.findAll().toList()
-    
+class ToDoListController(
+    private val repository: ToDoListRepository,
+    @Suppress("SpringJavaInjectionPointsAutowiringInspection") private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
+
+    @GetMapping
+    suspend fun getList(): List<ToDoList> = withContext(dispatcher) { repository.findAll().toList() }
+
 }
